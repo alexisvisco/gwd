2ebbd17d1f9cc46a7beba006ab8c8e572f242faa
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2022-08-25 10:47:18 +0200

    profile: only profile if GWD_PROFILE is set

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"github.com/alexisvisco/gwd/commands"
+	_ "net/http/pprof"
 	"os"
 	"runtime/pprof"
 )
-import _ "net/http/pprof"
 
 func main() {
-	f, err := os.Create("gwd.cpu.pprof")
-	if err != nil {
-		panic(err)
-	}
+	if os.Getenv("GWD_PROFILE") != "" {
+		f, err := os.Create("gwd.cpu.pprof")
+		if err != nil {
+			panic(err)
+		}
 
-	if err := pprof.StartCPUProfile(f); err != nil {
-		panic(err)
+		defer func() {
+			if err := pprof.StartCPUProfile(f); err != nil {
+				panic(err)
+			}
+		}()
 	}
 
 	commands.Execute()
-
-	pprof.StopCPUProfile()
 }
